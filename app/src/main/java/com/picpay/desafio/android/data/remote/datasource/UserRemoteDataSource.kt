bd4effcf1b0eb6f6f7becf3336ba0e1e71e98835
Sara Alves <sara.rodrigues@picpay.com>
2023-04-09 22:01:38 -0300

    delteando files para abrir um PR mais enxuto

--- app/src/main/java/com/picpay/desafio/android/data/remote/datasource/UserRemoteDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.picpay.desafio.android.data.remote.datasource
-
-import com.picpay.desafio.android.domain.model.response.User
-import kotlinx.coroutines.flow.Flow
-
-interface UserRemoteDataSource {
-    fun getUsers(): Flow<List<User>>
-}
--- app/src/main/java/com/picpay/desafio/android/domain/model/response/User.kt
+++
@@ -1,8 +0,0 @@
-package com.picpay.desafio.android.domain.model.response
-
-data class User(
-    val img: String,
-    val name: String,
-    val id: Int,
-    val username: String
-)
